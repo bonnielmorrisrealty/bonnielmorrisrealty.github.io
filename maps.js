4572ee77c5f47800f51e3020f754a95b610f0ce9
MOFNY <chrisschuck@yahoo.com>
2015-06-01 06:16:18 -0400

    Various revisions

--- maps.js
+++ maps.js
@@ -2,13 +2,33 @@ $(document).ready(function() {
   var mapCanvas = document.getElementById('map-canvas');
 function initialize() {
   var LatLngObj;
+  var contentString = '';
   if (mapCanvas.dataset.map === "lakeside-29") {
-    LatLngObj = {lat: 44.279491, lng: -73.979871};
+    LatLngObj = {lat: 44.312425, lng: -74.002874};
+    contentString = '<div id="content">'+
+      '<div>339 Whiteface Inn Lane</div>' +
+      '<div>Whiteface Club &amp; Resort</div>' +
+      '<div>Lake Placid, NY 12946</div>' +
+      '<div><a href="https://www.google.com/maps/dir/Current+Location/44.312425,-74.002874">Directions</a></div>' +
+      '</div>';
   }
   else if (mapCanvas.dataset.map === "haystack-way") {
     LatLngObj = {lat: 44.301413, lng: -74.015083};
+    contentString = '<div id="content">'+
+      '<div>32 Haystack Way</div>' +
+      '<div>Lake Placid, NY 12946</div>' +
+      '<div><a href="https://www.google.com/maps/dir/Current+Location/44.301413,-74.015083">Directions</a></div>' +
+      '</div>';
   }
-  
+  else if (mapCanvas.dataset.map === "dierview-brookhill") {
+    LatLngObj = {lat: 44.312351, lng: -74.004124};
+    contentString = '<div id="content">'+
+       '<div>316 Whiteface Inn Lane</div>' +
+      '<div>Whiteface Club &amp; Resort</div>' +
+      '<div>Lake Placid, NY 12946</div>' +
+      '<div><a href="https://www.google.com/maps/dir/Current+Location/44.312351,-74.004124">Directions</a></div>' +
+      '</div>';
+  }
   var mapOptions = {
     zoom: 16,
     center: LatLngObj,
@@ -20,13 +40,7 @@ function initialize() {
       position: LatLngObj,
       map: map
   });
-  var contentString = '<div id="content">'+
-      '<div>339 Whiteface Inn Lane</div>' +
-      '<div>Whiteface Club &amp; Resort</div>' +
-      '<div>Lake Placid, NY 12946</div>' +
-      '<div><a href="https://www.google.com/maps/dir/Current+Location/44.279491,-73.979871">Directions</a></div>' +
-      '</div>';
-
+ 
   var infowindow = new google.maps.InfoWindow({
       content: contentString
   });
