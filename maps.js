cf5abe9d8205673f6c48e55fb98148a1a7db059a
Christopher Schuck <chrisschuck@yahoo.com>
2016-09-05 12:43:53 -0400

    Removes unnecessary Google Analytics varification.  Adds all information and assets for Hemlock Way.

--- maps.js
+++ maps.js
@@ -132,6 +132,15 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.282313,-73.979047">Directions</a></div>' +
       '</div>';
   }
+  else if (mapCanvas.dataset.map === "hemlock-way") {
+    LatLngObj = {lat: 44.311668, lng: -74.006866};
+    contentString = '<div id="content">'+
+      '<div>Whiteface Club & Resort</div>' +
+      '<div>4 Hemlock Way #7</div>' +
+      '<div>Lake Placid, NY 12946</div>' +
+      '<div><a href="https://www.google.com/maps/dir/Current+Location/44.311668,-74.006866">Directions</a></div>' +
+      '</div>';
+  }
   var mapOptions = {
     zoom: 16,
     center: LatLngObj,
