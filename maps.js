4f40980b0abb0451b9de667a9d1cf76ea9c95ab4
Christopher Schuck <chrisschuck@yahoo.com>
2016-11-13 15:37:02 -0500

    Updates some small presentational things.  Adds morningside moose lodge.  Updates a few properties to have lazy loaded images and proper src attributes.

--- maps.js
+++ maps.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 function initialize() {
   var LatLngObj;
   var contentString = '';
-  if (mapCanvas.dataset.map === "lakeside-29") {
+  var mapData = mapCanvas.dataset.map;
+  if (mapData === "lakeside-29") {
     LatLngObj = {lat: 44.312425, lng: -74.002874};
     contentString = '<div id="content">'+
       '<div>339 Whiteface Inn Lane</div>' +
@@ -12,7 +13,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.312425,-74.002874">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "haystack-pines") {
+  else if (mapData === "haystack-pines") {
     LatLngObj = {lat: 44.301413, lng: -74.015083};
     contentString = '<div id="content">'+
       '<div>32 Haystack Pines</div>' +
@@ -20,7 +21,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.301413,-74.015083">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "dierview-brookhill") {
+  else if (mapData === "dierview-brookhill") {
     LatLngObj = {lat: 44.312351, lng: -74.004124};
     contentString = '<div id="content">'+
        '<div>316 Whiteface Inn Lane</div>' +
@@ -29,7 +30,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.312351,-74.004124">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "brookside-cabin") {
+  else if (mapData === "brookside-cabin") {
     LatLngObj = {lat: 44.311574, lng: -74.004124};
     contentString = '<div id="content">'+
        '<div>352 Whiteface Inn Lane</div>' +
@@ -38,7 +39,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.311574,-74.003670">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "cozy-cone") {
+  else if (mapData === "cozy-cone") {
     LatLngObj = {lat: 44.311758, lng: -74.004124};
     contentString = '<div id="content">'+
        '<div>351 Whiteface Inn Lane</div>' +
@@ -47,7 +48,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.311758,-74.002847">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "owls-nest") {
+  else if (mapData === "owls-nest") {
     LatLngObj = {lat: 44.314559, lng: -74.004940};
     contentString = '<div id="content">'+
        '<div>13 Overlook Way</div>' +
@@ -56,7 +57,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.314559,-74.004940">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "lakeside-16") {
+  else if (mapData === "lakeside-16") {
     LatLngObj = {lat: 44.312425, lng: -74.002874};
     contentString = '<div id="content">'+
        '<div>339 Whiteface Inn Lane</div>' +
@@ -65,7 +66,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.312425,-74.002874">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "balsams-townhome") {
+  else if (mapData === "balsams-townhome") {
     LatLngObj = {lat: 44.266220, lng: -73.977010};
     contentString = '<div id="content">'+
        '<div>16 Rustic Way</div>' +
@@ -73,7 +74,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.266220,-73.977010">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "sandbrook") {
+  else if (mapData === "sandbrook") {
     LatLngObj = {lat: 44.266220, lng: -73.977010};
     contentString = '<div id="content">'+
        '<div>22 Hemlock Way</div>' +
@@ -82,7 +83,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.311867,-74.005353">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "knollwood-cabin") {
+  else if (mapData === "knollwood-cabin") {
     LatLngObj = {lat: 44.279491, lng: -73.979871};
     contentString = '<div id="content">'+
        '<div>Whiteface Club &amp; Resort</div>' +
@@ -90,7 +91,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.279491,-73.979871">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "lakeside-9") {
+  else if (mapData === "lakeside-9") {
     LatLngObj = {lat: 44.312937, lng: -74.002791};
     contentString = '<div id="content">'+
        '<div>331 Whiteface Inn Ln.</div>' +
@@ -99,7 +100,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.312937,-74.002791">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "camp-brady") {
+  else if (mapData === "camp-brady") {
     LatLngObj = {lat: 44.311574, lng: -74.003670};
     contentString = '<div id="content">'+
       '<div>352 Whiteface Inn Ln.</div>' +
@@ -108,7 +109,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.311574,-74.003670">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "camp-tarshish") {
+  else if (mapData === "camp-tarshish") {
     LatLngObj = {lat: 44.117374, lng: -73.427161};
     contentString = '<div id="content">'+
       '<div>106 Presbury Point Rd</div>' +
@@ -116,7 +117,7 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.117374,-73.427161">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "cummings-road-complex") {
+  else if (mapData === "cummings-road-complex") {
     LatLngObj = {lat: 44.281072, lng: -73.987814};
     contentString = '<div id="content">'+
       '<div>52 Cummings Rd</div>' +
@@ -124,21 +125,29 @@ function initialize() {
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.281072,-73.987814">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "morningside-lodge") {
+  else if (mapData === "morningside-lodge") {
     LatLngObj = {lat: 44.282313, lng: -73.979047};
     contentString = '<div id="content">'+
-      '<div>20 Dunn Way, #45</div>' +
+      '<div>20 Dunn Way, #42</div>' +
       '<div>Lake Placid, NY 12946</div>' +
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.282313,-73.979047">Directions</a></div>' +
       '</div>';
   }
-  else if (mapCanvas.dataset.map === "hemlock-way") {
+  else if (mapData === "hemlock-way") {
     LatLngObj = {lat: 44.311668, lng: -74.006866};
     contentString = '<div id="content">'+
       '<div>Whiteface Club & Resort</div>' +
       '<div>4 Hemlock Way #7</div>' +
       '<div>Lake Placid, NY 12946</div>' +
       '<div><a href="https://www.google.com/maps/dir/Current+Location/44.311668,-74.006866">Directions</a></div>' +
+      '</div>';
+  }
+  else if (mapData === "morningside-moose-lodge") {
+    LatLngObj = {lat: 44.282313, lng: -73.979047};
+    contentString = '<div id="content">'+
+      '<div>20 Dunn Way, #45</div>' +
+      '<div>Lake Placid, NY 12946</div>' +
+      '<div><a href="https://www.google.com/maps/dir/Current+Location/44.282313,-73.979047">Directions</a></div>' +
       '</div>';
   }
   var mapOptions = {
